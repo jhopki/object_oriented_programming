2569f2f25ad5434354c6d1b5800434d084c4d8fe
Julia Hopkins <jfhopkins6@gmail.com>
2013-10-24 13:27:46 -0400

    try to display output

--- sales_tax.rb
+++ sales_tax.rb
@@ -21,7 +21,10 @@ class Product
     (@price + sales_tax)
   end
 
-  
+  def display_total
+    item_total = total_price/100.0
+    puts "#{@name}: #{item_total}"
+  end
 end
 
 class Imported < Product
@@ -43,16 +46,49 @@ class ExemptImport < Exempt
 end
 
 
-product = Product.new("music_cd", 14.99)
-puts product.sales_tax
-puts product.total_price
-imports = Imported.new("bottle of perfume", 27.99)
-puts imports.sales_tax
-puts imports.total_price
-exempt_item = Exempt.new("chocolate bar", 0.85)
-puts exempt_item.sales_tax
-puts exempt_item.total_price
-exemptimport = ExemptImport.new("box of chocolates", 11.25)
-puts exemptimport.sales_tax
-puts exemptimport.total_price
+# product = Product.new("music_cd", 14.99)
+# puts product.sales_tax
+# puts product.total_price
+# imports = Imported.new("bottle of perfume", 27.99)
+# puts imports.sales_tax
+# puts imports.total_price
+# exempt_item = Exempt.new("chocolate bar", 0.85)
+# puts exempt_item.sales_tax
+# puts exempt_item.total_price
+# exemptimport = ExemptImport.new("box of chocolates", 11.25)
+# puts exemptimport.sales_tax
+# puts sprintf('%.2f',((exemptimport.total_price)/100.0))
+
+# puts exemptimport.display_total(exempt_item.sales_tax)
+
+list1 = {"book" => 12.49, "music CD" => 14.99, "chocolate bar" => 0.85}
+list2 = {"imported box of chocolates" => 10.00, "imported bottle of perfume" => 47.50}
+list3 = {"imported bottle of perfume" => 27.99, "bottle of perfume" => 18.99, "packet of headache pills" => 9.75, "imported box of chocolates" => 11.25}
+
+
+
+def some_method(some_hash)
+  exempt_items = ["book", "chocolate bar", "packet of headache pills"]
+  imported_items = ["imported bottle of perfume"]
+  exempt_imports = ["imported box of chocolates"]
+
+  some_hash.each do |k,v| 
+    if exempt_items.include?(k)
+      new_item = Exempt.new(k,v)
+    elsif imported_items.include?(k)
+      new_item = Imported.new(k,v)
+    elsif exempt_imports.include?(k)
+      new_item = ExemptImport.new(k,v)
+    else
+      new_item = Product.new(k,v)
+    end
+      
+    puts new_item.sales_tax
+    puts new_item.total_price
+    puts new_item.display_total
+  end
+end
+
+some_method(list1)
+
 
