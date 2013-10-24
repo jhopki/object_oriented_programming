47a769086597806ce738006f074e305aa6ea6b8a
Julia Hopkins <jfhopkins6@gmail.com>
2013-10-24 17:15:49 -0400

    Rover exercise

--- rover.rb
+++ rover.rb
@@ -1,64 +1,61 @@
 
 class Rover
-  
+  @@directions = ["N","E","S","W"]
   def initialize(init_str)
     @init_str = init_str.split(" ")
-    @start_position = @init_str[0,2]
-    @init_heading = @init_str[2]
-  end
-  def initial_heading
-    @init_heading
+    @start_position = @init_str[0,2].map {|x| x.to_i}
+    @heading = @init_str[2]
   end
 
   def starting_position
     @start_position
   end
-
   def read_route(route)
     route.split("")
   end
 
-  def move_one(direction)
-    if direction == "N"
-      @start_position[1] += 1
-    elsif direction == "E"
-      @start_position[0] += 1
-    elsif direction == "W" 
-      @start_position[0] -= 1
-    elsif direction == "S" 
-      @start_position[1] -= 1
+  def move_one(position)
+    if @@directions[0] == "N"
+      position[1] += 1
+    elsif @@directions[0] == "E"
+      position[0] += 1
+    elsif @@directions[0] == "W" 
+      position[0] -= 1
+    elsif @@directions[0] == "S" 
+      position[1] -= 1
     else
       puts "error"      
     end
+    return position
   end
   
   
-  def turn(heading, direction_change)
-    if direction_change == "L"
-      new_heading = "W" if heading == "N"
-      new_heading = "S" if heading == "W"
-      new_heading = "E" if heading == "S"
-      new_heading = "N" if heading == "E"
-    end
-    if direction_change == "R"
-      new_heading = "E" if heading == "N"
-      new_heading = "N" if heading == "W"
-      new_heading = "W" if heading == "S"
-      new_heading = "S" if heading == "E"
-    end
-  end
+  # def turn(heading, direction_change)
+    # if direction_change == "L"
+      # new_heading = "W" if heading == "N"
+      # new_heading = "S" if heading == "W"
+    #   new_heading = "E" if heading == "S"
+    #   new_heading = "N" if heading == "E"
+    # end
+    # if direction_change == "R"
+    #   new_heading = "E" if heading == "N"
+  #     new_heading = "N" if heading == "W"
+  #     new_heading = "W" if heading == "S"
+  #     new_heading = "S" if heading == "E"
+  #   end
+  # end
   def new_turn(direction_change)
-    directions = ["N","E","S","W"]
+    
     if direction_change == "R"
-      directions.rotate!(-1)
+      @@directions.rotate!(-1)
     elsif direction_change == "L"
-      directions.rotate!(1)
+      @@directions.rotate!(1)
     else
       puts "error"
     end
-      new_heading = directions[0]
+      new_heading = @@directions[0]
   end
-
+end
 # INPUT AND OUTPUT
  
 # Test Input:
@@ -73,14 +70,21 @@ class Rover
 # 5 1 E
 
 rover1 = Rover.new("1 2 N")
-rover_route = "LMLMLMLMM"
-puts rover1.initial_heading
-puts rover1.starting_position
-
-rover1.read_route(rover_route).each do |x|
+rover_route1 = "LMLMLMLMM"
+rover2 = Rover.new("3 3 E")
+rover_route2 = "MMRMMRMRRM"
+# puts rover1.read_route(rover_route)
+position = rover1.starting_position
+new_position =[]
+rover1.read_route(rover_route1).each do |x|
   if x == "M"
-    new_position = rover1.move_one
+    new_position = rover1.move_one(position)
+    puts new_position
   elsif x == "L" || x == "R"
-    new_heading = rover1.turn
-    
-if L or R turn 
+    new_heading = rover1.new_turn(x)
+    puts new_heading
+  end
+end
+
+puts new_position.inspect
+
